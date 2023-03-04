53f7008aab46adb32d47f7f1c3feb492834202b9
Erhan <erhantezer68@gmail.com>
2023-03-04 17:34:13 +0300

    home page finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-
-
+import AppointmentList from "../components/AppointmentList"
+import Doctors from "../components/Doctors"
 
 const Home = () => {
     
     return (
-        <div>Home</div>
+        <main className="text-center mt-2">
+            <h1 className="display-5 text-danger">TEZER HOSPITAL</h1>
+            <Doctors/>
+            <AppointmentList/>
+        </main>
     )
 }
 
