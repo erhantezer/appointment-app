638e623a357af2a008a79495e9b5fb6908c76c5f
Erhan <erhantezer68@gmail.com>
2023-03-05 00:22:05 +0300

    updated

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -13,7 +13,15 @@ const AppointmentList = () => {
             <div className='d-flex flex-column align-items-center'>
                 {!appointments.length && (<img src="./img/appointment.jpg" width="80%" alt="" />)} 
                 {appointments?.map((item) =>{
-                    
+                    const { id, patient, consulted, doctor, day } = item;
+                    return (
+                            <div
+                            key={id}
+                            className={consulted ? "appointments consulted" : "appointments "}
+                            >
+
+                            </div>
+                        )
                 })}
             </div>
         </Container>
