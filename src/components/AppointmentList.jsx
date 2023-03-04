f4a21d8791bcb15534d4ba962873ac804284e702
Erhan <erhantezer68@gmail.com>
2023-03-05 00:13:16 +0300

    appointment list updated

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useGlobalContext } from '../context/context'
+import { Container } from 'react-bootstrap';
 
 const AppointmentList = () => {
+    const { appointments, setAppointments } = useGlobalContext();
+
     return (
-        <div>AppointmentList</div>
+        <Container className='p-2'>
+            <h3 className='display-6 mb-2' style={{ color: "rgb(166,18,189)" }}>
+                Appointment List
+            </h3>
+            <div>
+                
+            </div>
+        </Container>
     )
 }
 
